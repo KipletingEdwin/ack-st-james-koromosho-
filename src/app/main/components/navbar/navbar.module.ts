c179b8d9fa50bb31dd563eeec1a54790a2fc4303
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-02-08 21:21:58 +0000

    add a navbar component

---
+++ src/app/main/components/navbar/navbar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { NavbarComponent } from './navbar.component';
+import { RouterLink } from '@angular/router';
+
+@NgModule({
+  imports: [ CommonModule, FormsModule, IonicModule, RouterLink],
+  declarations: [NavbarComponent ],
+  exports: [NavbarComponent ]
+})
+export class NavbarComponentModule {}
